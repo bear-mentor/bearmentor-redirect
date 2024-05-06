3970cd07947744b5fcbbf9d8a435df2201e08df1
M Haidar Hanif <git@mhaidarhanif.com>
2024-05-07 02:07:19 +0700

    feat: more redirect urls

--- app/configs/redirects.ts
+++ app/configs/redirects.ts
@@ -2,6 +2,18 @@ import { type ConfigRedirect } from "~/utils/redirect-route.server";
 
 export const configRedirects: ConfigRedirect[] = [
   {
+    path: "/programs",
+    url: "https://bearmentor.notion.site/Bearmentor-with-Haidar-735d1483405b40cba6e110cf1f861769",
+  },
+  {
+    path: "/onsite",
+    url: "https://bearmentor.notion.site/Bootcamp-Onsite-Full-Stack-Web-di-Bearmentor-7504b32517124e5eb0c47be2250d8573",
+  },
+  {
+    path: "/onsite-register",
+    url: "https://airtable.com/appC9J8JxF40niUx1/shrk8RsgG3Qb4fPkg",
+  },
+  {
     path: "/register",
     url: "https://airtable.com/appoep7dnVYMxASul/shrh3DfTIltyeWx4y",
   },
@@ -12,5 +24,45 @@ export const configRedirects: ConfigRedirect[] = [
   {
     path: "/payment",
     url: "https://airtable.com/appoep7dnVYMxASul/shr9IOu7OF4eGRKW8",
+  },
+  {
+    path: "/handbook",
+    url: "https://bearmentor.notion.site/Bearmentor-Bootcamp-Handbook-Indonesian-4cc3a2d64b4c40c89abe2c26a4c1b3c8",
+  },
+  {
+    path: "/profile",
+    url: "https://airtable.com/appy4zcfskMzSC5wl/shrPE08KkNU9rw2Lq",
+  },
+  {
+    path: "/submit",
+    url: "https://airtable.com/appy4zcfskMzSC5wl/shrq8dIBtkonS183s",
+  },
+  {
+    path: "/feedback",
+    url: "https://airtable.com/appy4zcfskMzSC5wl/shroUmtS2THxI1S2n",
+  },
+  {
+    path: "/hiring",
+    url: "https://bearmentor.notion.site/a3145866da3240e4950d66dbef4df70e",
+  },
+  {
+    path: "/hiring-apply",
+    url: "https://airtable.com/app0KqazHuNw11dLe/shrvWP9voob1v82MJ",
+  },
+  {
+    path: "/hiring-test",
+    url: "https://airtable.com/app0KqazHuNw11dLe/shrduSWQGQo803XLy",
+  },
+  {
+    path: "/hiring-payout",
+    url: "https://airtable.com/app0KqazHuNw11dLe/shrmbmU4O0WVZXhz7",
+  },
+  {
+    path: "/mentor-onboarding",
+    url: "https://bearmentor.notion.site/Mentor-Onboarding-30acd48c04bd40dab4c287a57f964576",
+  },
+  {
+    path: "/example",
+    url: "https://example.com",
   },
 ];
